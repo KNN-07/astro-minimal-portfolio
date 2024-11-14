7a384fb793de7eeac3887d9ae34835658ee25079
Nguyen Khac Trung Kien <55886589+KNN-07@users.noreply.github.com>
2024-11-14 20:07:27 +0700

    ??

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
