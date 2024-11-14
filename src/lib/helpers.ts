472ad6f0e7b67631ff17ec6f7ef15f7312382185
Nguyen Khac Trung Kien <trungkiennguyenkhac007@gmail.com>
2024-11-14 20:36:52 +0700

    Revert "randomshit"
    This reverts commit b20031d6f4717d4665460bc7a5ae90e3788d70b9.

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -19,14 +19,14 @@ export function formatTimeForItaly(date: Date): string {
     minute: "2-digit",
     second: "2-digit",
     hour12: true, // This will format the time in 12-hour format with AM/PM
-    timeZone: "Asia/Jakarta",
+    timeZone: "Europe/Rome",
   };
 
   let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
 
   // Append the time zone abbreviation. You can automate this with libraries like `moment-timezone`.
   // For simplicity, here I'm just appending "CET", but do remember that Italy switches between CET and CEST.
-  formattedTime += "UTC+7";
+  formattedTime += " CET";
 
   return formattedTime;
 }
