0a5518e4042cd342ecf3f502b6e853916064abc7
Nguyen Khac Trung Kien <trungkiennguyenkhac007@gmail.com>
2024-11-14 21:52:59 +0700

    chore(overall): replace info and fix pipeline

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,18 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
+  facebook: "https://www.facebook.com/lostdrip.nktrgkien/",
+  instagram: "https://www.instagram.com/normalpersonidk/",
+  linkedin: "https://www.linkedin.com/in/trung-kien-nguyen-khac-3955a52a6/",
+  twitter: "https://twitter.com/normankr07",
+  github: "https://github.com/KNN-07",
+  gitlab: "https://gitlab.com/KNN-07",
+  email: "mailto:nktrungkien@protonmail.com",
+  reddit: "https://www.reddit.com/user/normankr07",
+  codeforces: "https://codeforces.com/profile/normankr07",
+  youtube: "https://www.youtube.com/@normalperson3095",
+  artstation: "https://www.artstation.com/normankr07",
+  deviantart: "https://www.deviantart.com/normank07",
+  anilist: "https://anilist.co/user/normankr07/",
 };
-
 export const loaderAnimation = [
   ".loader",
   { opacity: [1, 0], pointerEvents: "none" },
